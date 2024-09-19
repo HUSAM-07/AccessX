b1f0c9c9d3e32e99c0c3db136b668c02c0bc7e13
Mohammed Husamuddin <uni.husam@gmail.com>
2024-09-19 17:36:58 +0400

    updated Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,6 +6,8 @@ const portalLinks = [
     { title: 'Uni Notes', url: 'https://uni-notes.netlify.app/', description: 'Find information across courses and their respective notes, contributed by individual students' },
     { title: 'Google DSC Resources', url: 'https://gdscbpdc.github.io/', description: 'Find information of technical workshops and events conducted by Google DSC BPDC' },
     { title: 'ACM lib Resources', url: 'https://openlib-cs.acmbpdc.org/', description: 'A library of resources provided by ACM BPDC' },
+    { title: 'MTC Resources', url: 'https://mtcbpdcdubai.github.io/Coursework-Resources-and-Links/', description: 'A set of resources provided by MTC' }
+
 ];
 
 const Dashboard: React.FC = () => {
